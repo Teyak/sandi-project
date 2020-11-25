a5fb5b44b0457fefcfe7d31c18418a2d77997889
Teyak <74811879+Teyak@users.noreply.github.com>
2020-11-25 09:31:25 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,59 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $green"sabar yah bro,, agak lama:)"
+echo
+echo $white 
+figlet "NANO"
+pkg install nano
+echo $red
+figlet "FIGLET"
+pkg install figlet
+echo $cyan 
+figlet "PYTHON"
+pkg install python
+echo $green 
+figlet "PYTHON2"
+pkg install python2
+echo $blue 
+figlet "COWSAY"
+pkg install cowsay
+echo $yellow
+figlet "RUBY"
+pkg install ruby
+gem install lolcat
+echo $purple
+figlet "TOILET"
+pkg install toilet
+echo $blue
+figlet "PHP"
+pkg install php
+echo $red 
+figlet "REQUES"
+pip install requests
+pip2 install requests
+echo $white 
+figlet "MECHAN"
+pip install mechanize
+pip2 install mechanize
+echo $cyan
+figlet "JQ"
+pkg install jq
+figlet "LOLCAT"
+pip install lolcat
+pip2 install lolcat
+
+sleep 0.03
+echo
+echo
+echo $green"memproses..."
+sleep 0.04
+echo
+echo
+echo $white"berhasill..!!"
